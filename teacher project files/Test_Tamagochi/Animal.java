b30c409b1130cafb75b79421caf97dc56f5325ad
forktheonlyone <forktheonlyone@gmail.com>
2023-09-05 23:44:17 +0900

    delete Tamagochi

--- teacher project files/Test_Tamagochi/Animal.java
+++
@@ -1,7 +0,0 @@
-public abstract class Animal {
-    protected abstract void makeSound();
-    protected abstract void play();
-    protected abstract void eat();
-    protected abstract void sleep();
-    protected abstract void special();
-}
